c7ec0142c4d27bc416a65dc242e7c66e2bb6fe71
ebbuni1023 <97073638+CodingJiyoung@users.noreply.github.com>
2026-03-03 17:34:29 -0500

    first commit

---
+++ backend/src/test/java/com/codehaja/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehaja.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
